3d746c9fadd6cb537f069e64ded204821ed33ccd
Fabio Chimanski <fabio.chimanski@gpb.local>
2026-01-21 07:19:58 -0300

    ex003

---
+++ Exercicios/ex003/ex003.py
@@ -0,0 +1,29 @@
+#Simulador de caixa eletronico
+
+
+"""
+Conta:
+atributos
+    nome
+    conta
+    saldo
+    data
+    hora
+
+metodos
+    sacar
+    transferir
+    consultar_saldo
+
+estado
+    nome
+    saldo
+
+"""
+
+class ContaBancaria:
+    """
+Cria uma conta bancária e permite fazer saques e depositos
+    """
+    def __init__(self):
+        self.id = 
