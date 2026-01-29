b3e9cce9cbe216ae6ab3a2f3a4474cbe31ec78c7
Fabio Chimanski <fabiochime@live.com>
2026-01-29 15:04:22 -0300

    Banco dados banco

--- PRATICA 001/MeuBanco/Banco_Dados/database.py
+++ PRATICA 001/MeuBanco/Banco_Dados/database.py
@@ -1,14 +1,36 @@
 import sqlite3
 
-conn = sqlite3.connect('banco.db')
 
-cursor = conn.cursor()
+banco = sqlite3.connect('banco_sistema.db')
+
+cursor = banco.cursor()
 
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS ContasAbertas (
-        conta INTEGER PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS Contas_bancarias (
+        conta INTEGER PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
         senha INTEGER NOT NULL,
         saldo REAL DEFAULT 0
     );
 """)
+
+
+def criar_conta(nome_usuario, senha_usuario):
+    cursor.execute("""
+                   INSERT INTO Contas_bancarias (nome, senha, saldo) VALUES (?, ?, ?0 ,(nome_usuario, senha_usuario) """)
+    banco.commit()
+    banco.close()
+    print(f'Bem vindo {nome_usuario}! Sua conta foi criada com sucesso')
+
+    
+def saldo():
+    pass
+
+def transferencia():
+    pass
+
+def saque():
+    pass
+
+def extrato():
+    pass
--- PRATICA 001/MeuBanco/main.py
+++ PRATICA 001/MeuBanco/main.py
@@ -1,5 +1,25 @@
 #Ponto de partida do software
 from modelos.conta import Conta
+import Banco_Dados.database as db
 
-contas = {}
-while
+opcao = ""
+
+while True:
+    print('BANCO')
+    print('================')
+    print('1 - cadastro: ')
+    print('2 - acessar conta: ')
+    print('0 - sair sistema')
+    print('================')
+    opcao = input('opção: ')
+    match opcao:
+        case "1":
+            print('Bem vindo!')
+            nome_usuario = input('Informe seu nome: ')
+            senha_usuario = input('Informe sua senha usando apenas 4 digitos')
+            criar_conta(nome_usuario, senha_usuario)
+        case "2":
+            pass
+        case "0":
+            print('Até logo!')
+            break
