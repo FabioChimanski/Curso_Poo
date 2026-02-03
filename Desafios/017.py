25285732fca2a244f7b39c50c3d775231dce3ce2
Fabio Chimanski <fabiochime@live.com>
2026-02-03 16:11:32 -0300

    desafio 017

---
+++ Desafios/017.py
@@ -0,0 +1,28 @@
+from rich import print
+from rich.panel import Panel
+from rich.text import Text
+
+class Produto:
+    def __init__(self, nome, valor):
+        self.produto = nome
+        self.valor = valor
+
+    def etiqueta(self):
+
+        conteudo = f'{self.produto}\n -----------------------------------\n..........{self.valor}..........'
+        
+        conteudo_centralizado = aling.center(conteudo)
+        caixa = Panel(
+            conteudo_centralizado, 
+            title="Produto", 
+            width=40)
+        
+        print(caixa)
+
+
+
+p1 = Produto("iPhone 17 Pro Max", 25_00.85)
+p2 = Produto("Notebook Gamer", 8_000.00)
+
+p1.etiqueta()
+p2.etiqueta()
